8f09d4c0481f01fc968f5befe91060dd1e8c529e
MarcusRegan <mregan014@gmail.com>
2024-10-08 15:03:29 -0400

    lab 5 done

--- labs/lab-5/app.py
+++ labs/lab-5/app.py
@@ -7,6 +7,15 @@ from db.server import app
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/GUI2')
+def GUI2():
+    return render_template("GUI2.html")
+@app.route('/GUI3')
+def GUI3():
+    return render_template("GUI3.html")
+@app.route('/notit')
+def notit():
+    return render_template("notit.html")
 
 if __name__ == "__main__":
     # debug refreshes your application with your new changes every time you save
